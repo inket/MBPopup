f6f92be87677a2fc06dc93eb88cfd09db242cc13
Mahdi Bchetnia <mahdi@outlook.com>
2016-08-25 18:41:28 +0900

    Added example project

---
+++ Example/Example/AppDelegate.swift
@@ -0,0 +1,61 @@
+//
+//  AppDelegate.swift
+//  Example
+//
+//  Created by Mahdi Bchetnia on 16/8/25.
+//  Copyright © 2016 Mahdi Bchetnia. See LICENSE.
+//
+
+import Cocoa
+import MBPopup
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    let popupController: MBPopupController
+    let myView = NSView(frame: CGRect(x: 0, y: 0, width: 300, height: 400))
+    let label = NSTextField(frame: CGRect(x: 50, y: 175, width: 200, height: 50))
+
+    override init() {
+        self.popupController = MBPopupController(contentView: myView)
+        super.init()
+    }
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        popupController.statusItem.title = "Popup"
+        popupController.statusItem.length = 48
+        popupController.backgroundView.backgroundColor = NSColor.windowBackgroundColor
+
+        popupController.willOpenPopup = { keys in
+            var labelText = "Hi!"
+
+            switch keys {
+            case .option:
+                debugPrint("Will open popup with option pressed!")
+                labelText = "Hi, option user ;)"
+            case .shift:
+                debugPrint("Will open popup with shift pressed!")
+            case .shiftOption:
+                debugPrint("Will open popup with shift+option pressed!")
+            case .none:
+                debugPrint("Will open popup!")
+            }
+
+            self.label.stringValue = labelText
+        }
+
+        popupController.didOpenPopup = { debugPrint("Opened popup!") }
+        popupController.willClosePopup = { debugPrint("Will close popup!") }
+        popupController.didClosePopup = { debugPrint("Closed popup!") }
+
+        label.font = NSFont.systemFont(ofSize: 24)
+        label.isBezeled = false
+        label.drawsBackground = false
+        label.isEditable = false
+        label.isSelectable = false
+        label.alignment = NSTextAlignment.center
+        myView.addSubview(label)
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {}
+}
+
